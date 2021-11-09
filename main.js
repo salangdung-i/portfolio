6d42a4e75f6c33c5c8ab3965e7166e853e7f8e73
salangdung-i <dladmswl92@naver.com>
2021-11-09 15:50:13 +0900

    js : function scrollIntoView

--- main.js
+++ main.js
@@ -20,24 +20,16 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
-
+  scrollIntoView(link);
 });
 
 // Handle scrolling when tapping on ContactMe button
 const contactBtn = document.querySelector('.home__contact');
 contactBtn.addEventListener('click', (event) => {
-  const target = event.target;
-  const link = target.dataset.link;
-
-  if (link == null) {
-    return;
-  }
-
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
-
+  scrollIntoView('#contact');
 });
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
